18af5cfc773084115d7bfee95791e3d75d7a643e
kahonen <kristin.ahonen@gmail.com>
2018-01-28 22:53:12 -0500

    initial setup of files, functions and if/else

---
+++ liri.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+var random = require("./random.txt");
+var userCommand = process.argv[2];
+var userPick = process.argv[3];
+
+if (userCommand === "my-tweets"){
+    myTweets();
+}
+else if(userCommand === "spotify-this-song"){
+    spotifySong(userPick);
+}
+else if(userCommand === "movie-this"){
+    movieThis(userPick);
+}
+else if(userCommand === "do-what-it-says"){
+    doSays();
+};
+
+function myTweets(){
+
+    var client = new Twitter(keys.twitter);
+
+};
+
+function spotifySong(){
+
+    
+};
+
+function movieThis(){
+
+
+};
+
+function doSays(){
+
+
+};
